5d85543adcd8c623c4798ba9301b382436604365
Bryan Garcia <garciagg2193@gmail.com>
2026-09-18 19:53:23 -0600

    Fix migration rollback order for events

--- database/migrations/2026_01_18_213349_create_events_table.php
+++ database/migrations/2026_01_18_213349_create_events_table.php
@@ -40,6 +40,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('event_user');
         Schema::dropIfExists('events');
     }
 };

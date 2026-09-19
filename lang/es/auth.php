2db2ff08f0de1161ffd1e056f0099f80dc93474d
Bryan Garcia <garciagg2193@gmail.com>
2026-09-18 20:34:37 -0600

    Add Spanish translations and update logo

---
+++ lang/es/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Correo o contraseña incorrectos.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
